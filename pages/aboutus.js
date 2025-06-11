3b0ddf41dd7f2caa4d87b02a8252c168a50d7acc
Sonika Deogharia <deoghariasonika@gmail.com>
2025-06-11 20:30:38 +0530

    first project

---
+++ pages/aboutus.js
@@ -0,0 +1,13 @@
+// pages/aboutus.js
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Us Page</h1>
+      <p>This is the About Us page using Next.js</p>
+    </div>
+  );
+};
+
+export default AboutUs;
