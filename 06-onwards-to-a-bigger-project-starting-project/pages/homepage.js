711a35953948db4f20bc084ae7f46703b01b4262
Sonika Deogharia <deoghariasonika@gmail.com>
2025-06-13 10:42:45 +0530

    seo friendly website

---
+++ 06-onwards-to-a-bigger-project-starting-project/pages/homepage.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function HomePage() {
+  return (
+    <>
+      <Head>
+        <title>All Meetups</title>
+        <meta name="description" content="Browse a list of active React Meetups" />
+      </Head>
+      <div>...your page content...</div>
+    </>
+  );
+}
